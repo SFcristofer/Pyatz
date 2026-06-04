923a7e80b0f265ce2215762dc826cd58ab3fb505
SFcristofer <cristofer.contreras@technovalue.com.mx>
2026-06-04 10:21:21 -0600

    Revert "Mejora: Implementación de Editor de Texto Enriquecido en Detalle Técnico Especializado"
    This reverts commit ea9c515b6e572a23b8f8be73dd5f4779ff5907fa.

--- force-app/main/default/lwc/techQuoteItemConfigurator/techQuoteItemConfigurator.js
+++ force-app/main/default/lwc/techQuoteItemConfigurator/techQuoteItemConfigurator.js
@@ -92,13 +92,16 @@ export default class TechQuoteItemConfigurator extends LightningElement {
     get discountOptions() { return [ { label: '$', value: 'monto' }, { label: '%', value: 'porcentaje' } ]; }
 
     formatDescription(text) {
-        return text || '';
+        if (!text) return '';
+        let cleanText = text.replace(/<br\s*\/?>/gi, '\n');
+        cleanText = cleanText.replace(/<[^>]*>?/gm, '');
+        return cleanText;
     }
 
     loadEditData(item) {
         this.selectedPbeId = item.pbeId;
         this.selectedProductName = item.descripcion;
-        this.modalDescription = item.detalleTecnico || '';
+        this.modalDescription = this.formatDescription(item.detalleTecnico);
         const rawAreas = item.areas ? item.areas.split(',').map(a => a.trim()).filter(a => a !== '') : [];
         this.zonasAfectadas = [...new Set(rawAreas)];
         if (this.allHistoricalZones && this.allHistoricalZones.length > 0) this.normalizeExistingZones();
@@ -137,7 +140,7 @@ export default class TechQuoteItemConfigurator extends LightningElement {
         const res = this.searchResults.find(x => x.id === selectedId);
         if (res) {
             this.selectedPbeId = selectedId; this.selectedProductId = res.productId; this.selectedProductName = res.name; this.selectedProductPrice = res.unitPrice;
-            this.modalDescription = res.description || ''; this.searchResults = []; this.loadProductPrices(); this.initModalTable();
+            this.modalDescription = this.formatDescription(res.description); this.searchResults = []; this.loadProductPrices(); this.initModalTable();
         }
     }
 
